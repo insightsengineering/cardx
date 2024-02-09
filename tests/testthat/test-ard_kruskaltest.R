c84d4ff482d5a2fdc34e89ed9eabbf1850d9db36
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-02-09 18:44:55 +0000

    missed one numeric variable swap

--- tests/testthat/test-ard_kruskaltest.R
+++ tests/testthat/test-ard_kruskaltest.R
@@ -9,7 +9,7 @@ test_that("ard_kurskaltest() works", {
   expect_equal(
     ard_kruskaltest |>
       cards::get_ard_statistics(stat_name %in% c("statistic", "p.value")),
-    with(cards::ADSL, kruskal.test(AGEGR1, ARM)) |>
+    with(cards::ADSL, kruskal.test(AGEG, ARM)) |>
       broom::tidy() |>
       dplyr::select(statistic, p.value) |>
       unclass(),
