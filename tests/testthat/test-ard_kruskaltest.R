c5513250d46972ca30aed344c62314cb413f9f1d
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-02-12 15:31:05 +0000

    fix typo on KW test

--- tests/testthat/test-ard_kruskaltest.R
+++ tests/testthat/test-ard_kruskaltest.R
@@ -9,7 +9,7 @@ test_that("ard_kurskaltest() works", {
   expect_equal(
     ard_kruskaltest |>
       cards::get_ard_statistics(stat_name %in% c("statistic", "p.value")),
-    with(cards::ADSL, kruskal.test(AGEG, ARM)) |>
+    with(cards::ADSL, kruskal.test(AGE, ARM)) |>
       broom::tidy() |>
       dplyr::select(statistic, p.value) |>
       unclass(),
