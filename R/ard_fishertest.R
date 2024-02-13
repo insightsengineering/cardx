596941685811b7d3d70be0dcdb81027de9fff489
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-02-13 14:42:05 -0800

    adding `ard_fishertest()` (#21)
    **What changes are proposed in this pull request?**
    Migrated `ard_fishertest()` from {cards}
    
    
    
    --------------------------------------------------------------------------------
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [ ] Ensure all package dependencies are installed:
    `devtools::install_dev_deps()`
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [ ] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    - [ ] `usethis::use_spell_check()` runs with no spelling errors in
    documentation
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] Increment the version number using `usethis::use_version(which =
    "dev")`
    - [ ] Run `usethis::use_spell_check()` again
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge".
    
    ---------
    
    Signed-off-by: Daniel Sjoberg <danield.sjoberg@gmail.com>
    Signed-off-by: Zelos Zhu <zelos.zhu@atorusresearch.com>
    Co-authored-by: Zelos Zhu <zelos.zhu@atorusresearch.com>

---
+++ R/ard_fishertest.R
@@ -0,0 +1,59 @@
+#' ARD Fisher's Exact Test
+#'
+#' @description
+#' Analysis results data for Fisher's Exact Test.
+#' Calculated with `fisher.test(x = data[[variable]], y = data[[by]], ...)`
+#'
+#'
+#' @param data (`data.frame`)\cr
+#'   a data frame.
+#' @param by,variable ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   column names to compare
+#' @param ... additional arguments passed to `fisher.test(...)`
+#'
+#' @return ARD data frame
+#' @export
+#'
+#' @examples
+#' cards::ADSL[1:30, ] |>
+#'   ard_fishertest(by = "ARM", variable = "AGEGR1")
+ard_fishertest <- function(data, by, variable, ...) {
+  # check installed packages ---------------------------------------------------
+  cards::check_pkg_installed("broom.helpers", reference_pkg = "cardx")
+
+  # check/process inputs -------------------------------------------------------
+  check_not_missing(data)
+  check_not_missing(variable)
+  check_not_missing(by)
+  check_class_data_frame(x = data)
+  cards::process_selectors(data, by = {{ by }}, variable = {{ variable }})
+  check_scalar(by)
+  check_scalar(variable)
+
+  # build ARD ------------------------------------------------------------------
+  cards::tidy_as_ard(
+    lst_tidy =
+      cards::eval_capture_conditions(
+        stats::fisher.test(x = data[[variable]], y = data[[by]], ...) |>
+          broom::tidy()
+      ),
+    tidy_result_names =
+      c("estimate", "p.value", "conf.low", "conf.high", "method", "alternative"),
+    fun_args_to_record =
+      c(
+        "workspace", "hybrid", "hybridPars", "control", "or",
+        "conf.int", "conf.level", "simulate.p.value", "B"
+      ),
+    formals = formals(stats::fisher.test),
+    passed_args = dots_list(...),
+    lst_ard_columns = list(group1 = by, variable = variable, context = "fishertest")
+  ) |>
+    dplyr::mutate(
+      .after = "stat_name",
+      stat_label =
+        dplyr::case_when(
+          .data$stat_name %in% "p.value" ~ "p-value",
+          TRUE ~ .data$stat_name,
+        )
+    )
+}
