3c2c3f6ee796f2768e394e7ca467784f2515ae5b
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-02-21 20:17:20 -0500

    Update documentation

--- R/ard_survfit.R
+++ R/ard_survfit.R
@@ -6,96 +6,101 @@
 #'
 #' @param x ([survival::survfit()])\cr
 #'   a [survival::survfit()] object. See below for details.
-#' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   column name to compare by
-#' @param variable ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   column name to be compared
-#' @param id ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   column name of the subject or participant ID
+#' @param times (`numeric`)\cr
+#'   a vector of times for which to return survival probabilities.
+#' @param probs (`numeric`)\cr
+#'   a vector of probabilities with values in (0,1) specifying the survival quantiles to return.
+#' @param reverse (`logical`)\cr
+#'   Flip the probability reported, i.e. `1 - estimate`. Default is `FALSE`. Only applies when
+#'   `probs` is specified.
 #'
 #' @return an ARD data frame of class 'card'
-#' @name ard_surv_est
+#' @name ard_survfit
 #'
 #' @details
-#' For the `ard_surv_est()` function, the data is expected to be one row per subject.
-#' The data is passed as `t.test(data[[variable]] ~ data[[by]], paired = FALSE, ...)`.
-#'
-#' For the `ard_paired_ttest()` function, the data is expected to be one row
-#' per subject per by level. Before the t-test is calculated, the data are
-#' reshaped to a wide format to be one row per subject.
-#' The data are then passed as
-#' `t.test(x = data_wide[[<by level 1>]], y = data_wide[[<by level 2>]], paired = TRUE, ...)`.
+#' Only one of either the `times` or `probs` parameters can be specified.
 #'
 #' @examplesIf broom.helpers::.assert_package("survival", pkg_search = "cardx", boolean = TRUE)
 #' library(survival)
 #'
 #' survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE) |>
-#'   ard_surv_est(times = c(60, 180))
+#'   ard_survfit(times = c(60, 180))
 #'
 #' survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE) |>
-#'   ard_surv_est(probs = c(0.25, 0.5, 0.75))
+#'   ard_survfit(probs = c(0.25, 0.5, 0.75))
 NULL
 
-#' @rdname ard_surv_est
+#' @rdname ard_survfit
 #' @export
-ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
+ard_survfit <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
   # check installed packages ---------------------------------------------------
   cards::check_pkg_installed("survival", reference_pkg = "cardx")
 
-  # input checks ---------------------------------------------------------------
-  if (!all(inherits(x, "survfit"))) {
-    stop("Argument `x=` must be class 'survfit' created from the `survival::survfit()` function.",
-         call. = FALSE
-    )
-  }
-  if (c(is.null(times), is.null(probs)) %>% sum() != 1) {
-    stop("One and only one of `times=` and `probs=` must be specified.", call. = FALSE)
-  }
-  if (reverse == TRUE && !is.null(probs)) {
-    rlang::inform("`reverse=TRUE` argument ignored for survival quantile estimation.")
-  }
-
   # check/process inputs -------------------------------------------------------
   check_not_missing(x)
+  check_range(probs, c(0, 1))
+  check_binary(reverse)
+
+  if (!all(inherits(x, "survfit"))) {
+    cli::cli_abort(
+      "The {.arg x} argument must be class {.cls survfit} created using the {.fun survival::survfit} function."
+    )
+  }
+  if (sum(is.null(times), is.null(probs)) != 1) {
+    cli::cli_abort(
+      "One and only one of {.arg times} and {.arg probs} must be specified."
+    )
+  }
+  if (reverse && !is.null(probs)) {
+    cli::cli_inform(
+      "The {.code reverse=TRUE} argument is ignored for survival quantile estimation."
+    )
+  }
 
   # build ARD ------------------------------------------------------------------
   est_type <- ifelse(is.null(probs), "times", "probs")
-  tidy_stats <- switch(
+  tidy_survfit <- switch(
     est_type,
-    "times" = .format_survfit_time(x, times, reverse),
-    "probs" = .format_survfit_probs(x, probs)
+    "times" = .process_survfit_time(x, times, reverse),
+    "probs" = .process_survfit_probs(x, probs)
   )
 
-  .format_surv_est_results(tidy_stats)
+  .format_survfit_results(tidy_survfit)
 }
 
-.format_survfit_time <- function(x, times, reverse) {
+#' Process Survival Fit For Time Estimates
+#'
+#' @inheritParams cards::tidy_as_ard
+#' @inheritParams ard_survfit
+#'
+#' @return a tibble
+#'
+#' @examples
+#' cardx:::.format_survfit_time(
+#'   survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE),
+#'   times = c(60, 180),
+#'   reverse = FALSE
+#' )
+#'
+#' @keywords internal
+.process_survfit_time <- function(x, times, reverse) {
   tidy <- broom::tidy(x)
 
   strata <- intersect("strata", names(tidy)) %>%
     list() %>%
     compact()
-  multi_state <- inherits(x, "survfitms")
-  if (multi_state == TRUE) {
-    # selecting state to show
-    state <- unique(tidy$state) %>%
-      setdiff("(s0)") %>%
-      purrr::pluck(1)
-
-    tidy <- dplyr::filter(tidy, .data$state == .env$state)
-  }
 
   # adding time 0 to data frame
-  tidy <-
-    tidy %>%
-    # making strata a fct to preserve ordering
+  tidy <- tidy %>%
+    # make strata a fct to preserve ordering
     mutate_at(vars(strata), ~ factor(., levels = unique(.))) %>%
-    # if CI is missing, and SE is 0, making the CIs the estimate
+    # if CI is missing, and SE is 0, make the CI the estimate
     mutate_at(
       vars("conf.high", "conf.low"),
       ~ ifelse(is.na(.) & .data$std.error == 0, .data$estimate, .)
     ) %>%
     select(any_of(c("time", "estimate", "conf.high", "conf.low", "strata"))) %>%
+    # add data for time 0
     bind_rows(
       group_by(., strata) %>%
         slice(1) %>%
@@ -108,14 +113,13 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
     ) %>%
     ungroup()
 
-  # getting requested estimates
-  df_stat <-
-    tidy %>%
-    # getting the latest time (not showing estimates after that time)
+  # get requested estimates
+  df_stat <- tidy %>%
+    # find max time
     group_by(., strata) %>%
     mutate(time_max = max(.data$time)) %>%
     ungroup() %>%
-    # adding in timepoints requested by user
+    # add requested timepoints
     full_join(
       select(tidy, strata) %>%
         distinct() %>%
@@ -126,7 +130,7 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
         unnest(cols = c("time", "col_name")),
       by = unlist(c(strata, "time"))
     ) %>%
-    # if the user-specifed time is unobserved, filling estimates with previous value
+    # if user-specifed time is unobserved, fill estimate with previous value
     arrange(strata, .data$time) %>%
     group_by(strata) %>%
     tidyr::fill(
@@ -134,17 +138,17 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
       .direction = "down"
     ) %>%
     ungroup() %>%
-    # keeping obs of user-specified times
+    # keep only user-specified times
     filter(!is.na(.data$col_name)) %>%
-    # if user-specified time is after the latest follow-up time, making it NA
+    # if user-specified time is after max time, make estimate NA
     mutate_at(
       vars("estimate", "conf.high", "conf.low"),
       ~ ifelse(.data$time > .data$time_max, NA_real_, .)
     ) %>%
     select(-c(time_max, col_name))
 
-  # converting to reverse probs, if requested
-  if (reverse == TRUE) {
+  # reverse probs if requested
+  if (reverse) {
     df_stat <-
       df_stat %>%
       mutate_at(vars("estimate", "conf.low", "conf.high"), ~ 1 - .) %>%
@@ -154,23 +158,36 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
   df_stat
 }
 
-.format_survfit_probs <- function(x, probs) {
+#' Process Survival Fit For Quantile Estimates
+#'
+#' @inheritParams cards::tidy_as_ard
+#' @inheritParams ard_survfit
+#'
+#' @return a tibble
+#'
+#' @examples
+#' cardx:::.format_survfit_probs(
+#'   survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE),
+#'   probs = c(0.25, 0.5, 0.75)
+#' )
+#'
+#' @keywords internal
+.process_survfit_probs <- function(x, probs) {
   tidy <- broom::tidy(x)
 
   strata <- intersect("strata", names(tidy)) %>%
     list() %>%
     compact()
 
-  # calculating survival quantiles, and adding estimates to pretty tbl
+  # calculate survival quantiles and add estimates to df
   df_stat <- purrr::map2_dfr(
-    probs, seq_along(probs),
+    probs,
+    seq_along(probs),
     ~ stats::quantile(x, probs = .x) %>%
       as.data.frame() %>%
       tibble::rownames_to_column() %>%
       set_names(c("strata", "estimate", "conf.low", "conf.high")) %>%
-      mutate(
-        prob = .x
-      )
+      mutate(prob = .x)
   )
 
   df_stat
@@ -181,14 +198,15 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
 #' @inheritParams cards::tidy_as_ard
 #'
 #' @return an ARD data frame of class 'card'
-#' @keywords internal
 #'
 #' @examples
-#' cardx:::.format_surv_est_results(
+#' cardx:::.format_survfit_results(
 #'   broom::tidy(survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE))
 #' )
-.format_surv_est_results <- function(tidy_stats) {
-  ret <- tidy_stats %>%
+#'
+#' @keywords internal
+.format_survfit_results <- function(tidy_survfit) {
+  ret <- tidy_survfit %>%
     mutate(across(
       any_of(c("estimate", "conf.high", "conf.low", "time", "prob")), ~ as.list(.)
     )) %>%
@@ -199,10 +217,9 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
     ) %>%
     separate_wider_delim(strata, "=", names = c("variable", "variable_level"))
 
-  # summarize model ------------------------------------------------------------
   ret |>
     dplyr::left_join(
-      .df_survest_stat_labels(),
+      .df_survfit_stat_labels(),
       by = "stat_name"
     ) |>
     dplyr::mutate(
@@ -214,14 +231,13 @@ ard_surv_est <- function(x, times = NULL, probs = NULL, reverse = FALSE) {
           }
         ),
       context = "survival",
-  # add the stat label ---------------------------------------------------------
       stat_label = dplyr::coalesce(.data$stat_label, .data$stat_name)
     ) |>
     cards::tidy_ard_column_order() %>%
     structure(., class = c("card", class(.)))
 }
 
-.df_survest_stat_labels <- function() {
+.df_survfit_stat_labels <- function() {
   dplyr::tribble(
     ~stat_name, ~stat_label,
     "estimate", "Survival Probability",
