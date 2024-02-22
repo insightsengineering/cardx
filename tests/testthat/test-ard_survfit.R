7e2523ad630231ca614a705e622820a35aba561c
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-02-21 21:26:07 -0500

    Fix docs

--- tests/testthat/test-ard_survfit.R
+++ tests/testthat/test-ard_survfit.R
@@ -1,6 +1,6 @@
 test_that("ard_survfit() works with times provided", {
   expect_snapshot(
-    survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE) |>
+    survival::survfit(survival::Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE) |>
       ard_survfit(times = c(60, 180)) |>
       dplyr::mutate(
         stat = lapply(stat, function(x) ifelse(is.numeric(x), cards::round5(x, 3), x))
@@ -11,7 +11,7 @@ test_that("ard_survfit() works with times provided", {
 
 test_that("ard_survfit() works with probs provided", {
   expect_snapshot(
-    survfit(Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE) |>
+    survival::survfit(survival::Surv(AVAL, CNSR) ~ TRTA, cards::ADTTE) |>
       ard_survfit(probs = c(0.25, 0.75)) |>
       dplyr::mutate(
         stat = lapply(stat, function(x) ifelse(is.numeric(x), cards::round5(x, 3), x))
