160b55f75cd7f2796eea74696ff1f07252d5c305
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-03-11 13:49:59 -0700

    Updating to accept multiple variables

--- R/ard_ttest.R
+++ R/ard_ttest.R
@@ -7,8 +7,9 @@
 #'   a data frame. See below for details.
 #' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
 #'   column name to compare by
-#' @param variable ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   column name to be compared
+#' @param variables ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   column names to be compared. Independent t-tests will be computed for
+#'   each variable.
 #' @param id ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
 #'   column name of the subject or participant ID
 #' @param ... arguments passed to `t.test(...)`
@@ -29,7 +30,7 @@
 #' @examplesIf cards::is_pkg_installed("broom", reference_pkg = "cardx")
 #' cards::ADSL |>
 #'   dplyr::filter(ARM %in% c("Placebo", "Xanomeline High Dose")) |>
-#'   ard_ttest(by = ARM, variable = AGE)
+#'   ard_ttest(by = ARM, variable = c(AGE, BMIBL))
 #'
 #' # constructing a paired data set,
 #' # where patients receive both treatments
@@ -42,7 +43,7 @@ NULL
 
 #' @rdname ard_ttest
 #' @export
-ard_ttest <- function(data, by, variable, ...) {
+ard_ttest <- function(data, by, variables, ...) {
   # check installed packages ---------------------------------------------------
   cards::check_pkg_installed("broom", reference_pkg = "cardx")
 
@@ -52,27 +53,37 @@ ard_ttest <- function(data, by, variable, ...) {
   check_not_missing(by)
   check_data_frame(data)
   data <- dplyr::ungroup(data)
-  cards::process_selectors(data, by = {{ by }}, variable = {{ variable }})
+  cards::process_selectors(data, by = {{ by }}, variables = {{ variables }})
   check_scalar(by)
-  check_scalar(variable)
+
+  # if no variables selected, return empty tibble ------------------------------
+  if (is_empty(variables)) {
+    return(dplyr::tibble())
+  }
 
   # build ARD ------------------------------------------------------------------
-  .format_ttest_results(
-    by = by,
-    variable = variable,
-    lst_tidy =
-      cards::eval_capture_conditions(
-        stats::t.test(data[[variable]] ~ data[[by]], ...) |>
-          broom::tidy()
-      ),
-    paired = FALSE,
-    ...
-  )
+  lapply(
+    variables,
+    function(variable) {
+      .format_ttest_results(
+        by = by,
+        variable = variable,
+        lst_tidy =
+          cards::eval_capture_conditions(
+            stats::t.test(data[[variable]] ~ data[[by]], ...) |>
+              broom::tidy()
+          ),
+        paired = FALSE,
+        ...
+      )
+    }
+  ) |>
+    dplyr::bind_rows()
 }
 
 #' @rdname ard_ttest
 #' @export
-ard_paired_ttest <- function(data, by, variable, id, ...) {
+ard_paired_ttest <- function(data, by, variables, id, ...) {
   # check installed packages ---------------------------------------------------
   cards::check_pkg_installed("broom", reference_pkg = "cardx")
 
@@ -83,26 +94,36 @@ ard_paired_ttest <- function(data, by, variable, id, ...) {
   check_not_missing(id)
   check_data_frame(data)
   data <- dplyr::ungroup(data)
-  cards::process_selectors(data, by = {{ by }}, variable = {{ variable }}, id = {{ id }})
+  cards::process_selectors(data, by = {{ by }}, variables = {{ variables }}, id = {{ id }})
   check_scalar(by)
-  check_scalar(variable)
   check_scalar(id)
 
+  # if no variables selected, return empty tibble ------------------------------
+  if (is_empty(variables)) {
+    return(dplyr::tibble())
+  }
+
   # build ARD ------------------------------------------------------------------
-  .format_ttest_results(
-    by = by,
-    variable = variable,
-    lst_tidy =
-      cards::eval_capture_conditions({
-        # adding this reshape inside the eval, so if there is an error it's captured in the ARD object
-        data_wide <- .paired_data_pivot_wider(data, by = by, variable = variable, id = id)
-        # perform paired t-test
-        stats::t.test(x = data_wide[["by1"]], y = data_wide[["by2"]], paired = TRUE, ...) |>
-          broom::tidy()
-      }),
-    paired = TRUE,
-    ...
-  )
+  lapply(
+    variables,
+    function(variable) {
+      .format_ttest_results(
+        by = by,
+        variable = variable,
+        lst_tidy =
+          cards::eval_capture_conditions({
+            # adding this reshape inside the eval, so if there is an error it's captured in the ARD object
+            data_wide <- .paired_data_pivot_wider(data, by = by, variable = variable, id = id)
+            # perform paired t-test
+            stats::t.test(x = data_wide[["by1"]], y = data_wide[["by2"]], paired = TRUE, ...) |>
+              broom::tidy()
+          }),
+        paired = TRUE,
+        ...
+      )
+    }
+  ) |>
+    dplyr::bind_rows()
 }
 
 #' Convert t-test to ARD
