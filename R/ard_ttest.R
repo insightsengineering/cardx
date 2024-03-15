35285769de8ef440ef8ced3aebe4169ff450aae4
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-03-14 20:21:59 -0700

    Update R/ard_ttest.R
    thank you!
    
    Co-authored-by: Abinaya Yogasekaram <73252787+ayogasekaram@users.noreply.github.com>
    Signed-off-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

--- R/ard_ttest.R
+++ R/ard_ttest.R
@@ -6,7 +6,7 @@
 #' @param data (`data.frame`)\cr
 #'   a data frame. See below for details.
 #' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
-#'   column name to compare by
+#'   column name to compare by.
 #' @param variables ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
 #'   column names to be compared. Independent t-tests will be computed for
 #'   each variable.
