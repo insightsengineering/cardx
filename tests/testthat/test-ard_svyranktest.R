fe91fb7704f56cb43d9ace9d1758da50ef559ced
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-03-28 21:04:21 +0000

    working draft

---
+++ tests/testthat/test-ard_svyranktest.R
@@ -0,0 +1,37 @@
+skip_if_not(cards::is_pkg_installed(c("survey", "broom"), reference_pkg = "cardx"))
+
+test_that("ard_svyranktest() works", {
+  data(api, package = "survey")
+  dclus2 <- survey::svydesign(id = ~ dnum + snum, fpc = ~ fpc1 + fpc2, data = apiclus2)
+
+  expect_error(
+    lapply(
+      c("wilcoxon", "vanderWaerden", "median","KruskalWallis"),
+      function(x) {
+        ard_svyranktest(
+          dclus2,
+          variable = enroll,
+          by = comp.imp,
+          test = x
+        )
+      }),
+    NA
+  )
+})
+
+test_that("exact values match for ard_svyranktest works", {
+  svywilcox <- ard_svyranktest(
+    dclus2,
+    variable = enroll,
+    by = comp.imp,
+    test = "wilcoxon"
+  )
+  expect_equal(
+    cards::get_ard_statistics(
+      svywilcox,
+      stat_name %in% c("estimate", "p.value")
+    ),
+    survey::svyranktest(enroll ~ comp.imp, dclus2, test = "wilcoxon")[c("estimate", "p.value")],
+    ignore_attr = TRUE
+  )
+})
