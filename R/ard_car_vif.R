60f0c0d22c1d79a4e03dc37f05a43323ff976fe3
Abinaya Yogasekaram <73252787+ayogasekaram@users.noreply.github.com>
2024-04-12 00:06:55 -0400

    rename functions with new package conventions. (#110)
    **What changes are proposed in this pull request?**
    Functions are renamed according to new conventions:
    `ard_<pkgname>_<fnname>()` if a package specific function is used
    If a `.` appears in a function name it will be replaced with a `_` in
    the ard_*() function name.
    
    closes #106  @ayogasekaram 
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [x] **All** GitHub Action workflows pass with a :white_check_mark:
    - [x] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [x] If a bug was fixed, a unit test was added.
    - [x] If a new `ard_*()` function was added, it passes the ARD
    structural checks from `cards::check_ard_structure()`.
    - [x] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    - [x] Request a reviewer
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [x] If a bug was fixed, a unit test was added.
    - [x] Run `pkgdown::build_site()`. Check the R console for errors, and
    review the rendered website.
    - [x] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [x] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cards (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".
    
    ---------
    
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

---
+++ R/ard_car_vif.R
@@ -0,0 +1,99 @@
+#' Regression VIF ARD
+#'
+#' @description
+#' Function takes a regression model object and returns the variance inflation factor (VIF)
+#' using [`car::vif()`] and converts it to a ARD structure
+#'
+#' @param x regression model object
+#' See car::vif() for details
+#'
+#' @param ... arguments passed to `car::vif(...)`
+#'
+#' @return data frame
+#' @name ard_car_vif
+#' @rdname ard_car_vif
+#' @export
+#'
+#' @examples
+#' lm(AGE ~ ARM + SEX, data = cards::ADSL) |>
+#'   ard_car_vif()
+ard_car_vif <- function(x, ...) {
+  # check inputs ---------------------------------------------------------------
+  check_not_missing(x)
+
+  vif <- cards::eval_capture_conditions(car::vif(x, ...))
+
+  # if vif failed, set result as NULL, error will be kept through eval_capture_conditions()
+  if (is.null(vif$result)) {
+    # try to capture variable names from `terms()`
+    lst_terms <- cards::eval_capture_conditions(attr(stats::terms(x), "term.labels"))
+    # we cannot get variable names, error out
+    if (!is.null(lst_terms[["error"]])) {
+      cli::cli_abort(
+        c("There was an error running {.fun car::vif}. See below.", x = vif[["error"]])
+      )
+    }
+    vif$result <- dplyr::tibble(
+      variable = lst_terms[["result"]],
+      VIF = list(NULL),
+      GVIF = list(NULL),
+      aGVIF = list(NULL),
+      df = list(NULL)
+    )
+  }
+  # if VIF is returned
+  else if (!is.matrix(vif$result)) {
+    vif$result <- dplyr::tibble(variable = names(vif$result), VIF = vif$result)
+  }
+  # if Generalized VIF is returned
+  else if (is.matrix(vif$result)) {
+    vif$result <-
+      vif$result |>
+      as.data.frame() %>%
+      dplyr::mutate(., variable = rownames(.), .before = 1L) |>
+      dplyr::rename(
+        aGVIF = "GVIF^(1/(2*Df))",
+        df = "Df"
+      ) |>
+      dplyr::tibble()
+  }
+
+  # Clean-up the result to fit the ard structure through pivot
+  vif$result <-
+    vif$result |>
+    tidyr::pivot_longer(
+      cols = -c("variable"),
+      names_to = "stat_name",
+      values_to = "stat"
+    ) |>
+    dplyr::mutate(
+      context = "vif",
+      stat_label = ifelse(
+        .data$stat_name == "aGVIF",
+        "Adjusted GVIF",
+        .data$stat_name
+      ),
+      fmt_fn = map(
+        .data$stat,
+        function(.x) {
+          # styler: off
+          if (is.integer(.x)) return(0L)
+          if (is.numeric(.x)) return(1L)
+          # styler: on
+          NULL
+        }
+      )
+    )
+
+  # Bind the results and possible warning/errors together
+  vif_return <- dplyr::tibble(
+    vif$result,
+    warning = vif["warning"],
+    error = vif["error"]
+  )
+
+  # Clean up return object
+  vif_return |>
+    cards::tidy_ard_column_order() %>%
+    {structure(., class = c("card", class(.)))}  # styler: off
+}
