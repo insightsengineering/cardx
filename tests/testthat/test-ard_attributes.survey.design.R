6ee0e89fb8cc6913648c4fc313595c300abfbb89
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-10-03 18:58:24 -0700

    oh snap

--- tests/testthat/test-ard_attributes.survey.design.R
+++ tests/testthat/test-ard_attributes.survey.design.R
@@ -1,7 +1,7 @@
 skip_if_not(is_pkg_installed(c("survey", "withr"), reference_pkg = "cardx"))
 
 test_that("ard_attributes.survey.design() works", {
-  withr::local_options(list(width = 90))
+  withr::local_options(list(width = 120))
   data(api, package = "survey")
   dclus1 <- survey::svydesign(id = ~dnum, weights = ~pw, data = apiclus1, fpc = ~fpc)
 
