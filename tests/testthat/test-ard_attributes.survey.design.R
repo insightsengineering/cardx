8ea5ce7de288983032a31dfbf141a3ac3251599f
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-10-03 18:44:10 -0700

    bump version number for cards

--- tests/testthat/test-ard_attributes.survey.design.R
+++ tests/testthat/test-ard_attributes.survey.design.R
@@ -1,6 +1,7 @@
-skip_if_not(is_pkg_installed("survey", reference_pkg = "cardx"))
+skip_if_not(is_pkg_installed(c("survey", "withr"), reference_pkg = "cardx"))
 
 test_that("ard_attributes.survey.design() works", {
+  withr::local_options(list(width = 90))
   data(api, package = "survey")
   dclus1 <- survey::svydesign(id = ~dnum, weights = ~pw, data = apiclus1, fpc = ~fpc)
 
