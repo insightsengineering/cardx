678f56461bcf65070cd202ab7637d5e382c3d32d
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-01-03 13:23:21 -0800

    Adding `ard_categorical_ci(denominator)` argument

--- tests/testthat/test-ard_categorical_ci.data.frame.R
+++ tests/testthat/test-ard_categorical_ci.data.frame.R
@@ -1,6 +1,6 @@
-test_that("ard_categorical_ci.data.frame(method = 'wald')", {
-  skip_if_not(is_pkg_installed("broom"))
+skip_if_not(is_pkg_installed("broom"))
 
+test_that("ard_categorical_ci.data.frame(method = 'wald')", {
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -14,8 +14,6 @@ test_that("ard_categorical_ci.data.frame(method = 'wald')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'waldcc')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -29,8 +27,6 @@ test_that("ard_categorical_ci.data.frame(method = 'waldcc')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'clopper-pearson')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -44,8 +40,6 @@ test_that("ard_categorical_ci.data.frame(method = 'clopper-pearson')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'wilson')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -59,8 +53,6 @@ test_that("ard_categorical_ci.data.frame(method = 'wilson')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'wilsoncc')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -74,8 +66,6 @@ test_that("ard_categorical_ci.data.frame(method = 'wilsoncc')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'strat_wilson')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   mtcars$gear <- as.factor(mtcars$gear)
   expect_equal(
     ard_categorical_ci(
@@ -88,11 +78,45 @@ test_that("ard_categorical_ci.data.frame(method = 'strat_wilson')", {
     proportion_ci_strat_wilson(mtcars[["vs"]], strata = as.factor(mtcars[["gear"]])) |>
       unlist() |> unname()
   )
+
+  # check `denominator = "row"` result
+  expect_equal(
+    ard_categorical_ci(
+      mtcars |> dplyr::mutate(cyl = factor(cyl)),
+      variables = vs,
+      by = am,
+      strata = "cyl",
+      method = "strat_wilson",
+      denominator = "row"
+    ) |>
+      dplyr::filter(group1_level %in% 0) |>
+      cards::get_ard_statistics(),
+    proportion_ci_strat_wilson(
+      x = (mtcars$am == 0)[mtcars$vs == 1],
+      strata = as.factor(mtcars[["cyl"]])[mtcars$vs == 1]
+    )
+  )
+
+  # check `denominator = "cell"` result
+  expect_equal(
+    ard_categorical_ci(
+      mtcars |> dplyr::mutate(cyl = factor(cyl)),
+      variables = vs,
+      by = am,
+      strata = "cyl",
+      method = "strat_wilson",
+      denominator = "cell"
+    ) |>
+      dplyr::filter(group1_level %in% 0, variable_level %in% 1) |>
+      cards::get_ard_statistics(),
+    proportion_ci_strat_wilson(
+      x = mtcars$am == 0 & mtcars$vs == 1,
+      strata = as.factor(mtcars[["cyl"]])
+    )
+  )
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'strat_wilsoncc')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   mtcars$gear <- as.factor(mtcars$gear)
   expect_equal(
     ard_categorical_ci(
@@ -108,8 +132,6 @@ test_that("ard_categorical_ci.data.frame(method = 'strat_wilsoncc')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'jeffreys')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -123,8 +145,6 @@ test_that("ard_categorical_ci.data.frame(method = 'jeffreys')", {
 })
 
 test_that("ard_categorical_ci.data.frame(method = 'agresti-coull')", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_equal(
     ard_categorical_ci(
       mtcars,
@@ -138,8 +158,6 @@ test_that("ard_categorical_ci.data.frame(method = 'agresti-coull')", {
 })
 
 test_that("ard_continuous_ci.data.frame() follows ard structure", {
-  skip_if_not(is_pkg_installed("broom"))
-
   expect_silent(
     ard_categorical_ci(
       mtcars,
@@ -147,5 +165,115 @@ test_that("ard_continuous_ci.data.frame() follows ard structure", {
       method = "wald"
     ) |>
       cards::check_ard_structure()
+  )
+})
+
+test_that("ard_continuous_ci.data.frame(denominator='row')", {
+  # check the structure of the output
+  expect_silent(
+    ard <- ard_categorical_ci(
+      mtcars,
+      by = am,
+      variables = vs,
+      value = NULL,
+      denominator = "row"
+    )
+  )
+  expect_silent(
+    cards::check_ard_structure(ard)
+  )
+
+  # check the estimates align with `cards::ard_categorical(denominator='row)`
+  expect_equal(
+    ard |>
+      dplyr::filter(stat_name == "estimate") |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(unlist(group1_level), unlist(variable_level)),
+    cards::ard_categorical(
+      mtcars,
+      by = am,
+      variables = vs,
+      denominator = "row",
+      statistic = ~ "p"
+    ) |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(unlist(group1_level), unlist(variable_level))
+  )
+
+  # check the estimates align with `cards::ard_categorical(denominator='row)` for dichotomous variables
+  expect_equal(
+    ard_categorical_ci(
+      mtcars,
+      by = am,
+      variables = vs,
+      denominator = "row"
+    ) |>
+      dplyr::filter(stat_name == "estimate") |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(unlist(group1_level), unlist(variable_level)),
+    cards::ard_dichotomous(
+      mtcars,
+      by = am,
+      variables = vs,
+      denominator = "row",
+      statistic = ~ "p"
+    ) |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(unlist(group1_level), unlist(variable_level))
+  )
+})
+
+test_that("ard_continuous_ci.data.frame(denominator='cell')", {
+  # check the structure of the output
+  expect_silent(
+    ard <- ard_categorical_ci(
+      mtcars,
+      by = am,
+      variables = vs,
+      value = NULL,
+      denominator = "cell"
+    )
+  )
+  expect_silent(
+    cards::check_ard_structure(ard)
+  )
+
+  # check the estimates align with `cards::ard_categorical(denominator='row)`
+  expect_equal(
+    ard |>
+      dplyr::filter(stat_name == "estimate") |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(group1, variable, unlist(group1_level), unlist(variable_level)),
+    cards::ard_categorical(
+      mtcars,
+      by = am,
+      variables = vs,
+      denominator = "cell",
+      statistic = ~ "p"
+    ) |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(group1, variable, unlist(group1_level), unlist(variable_level))
+  )
+
+  # check the estimates align with `cards::ard_categorical(denominator='row)` for dichotomous variables
+  expect_equal(
+    ard_categorical_ci(
+      mtcars,
+      by = am,
+      variables = vs,
+      denominator = "cell"
+    ) |>
+      dplyr::filter(stat_name == "estimate") |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(unlist(group1_level), unlist(variable_level)),
+    cards::ard_dichotomous(
+      mtcars,
+      by = am,
+      variables = vs,
+      denominator = "cell",
+      statistic = ~ "p"
+    ) |>
+      dplyr::select(cards::all_ard_groups(), cards::all_ard_variables(), "stat") |>
+      dplyr::arrange(unlist(group1_level), unlist(variable_level))
   )
 })
