5ec02f8c732256c4323ff6f2811c6134523328ff
Daniel Sjoberg <danield.sjoberg@gmail.com>
2025-02-14 12:44:15 -0800

    better handling of NA values

--- R/ard_categorical_ci.R
+++ R/ard_categorical_ci.R
@@ -241,7 +241,7 @@ ard_categorical_ci.data.frame <- function(data,
 }
 
 .levels_for_row <- function(data, by) {
-  cards::nest_for_ard(data = data, by = by, include_data = FALSE) |>
+  suppressMessages(cards::nest_for_ard(data = data, by = by, include_data = FALSE)) |>
     dplyr::select(cards::all_ard_groups(types = "levels")) |>
     map(unlist) |>
     reduce(.f = \(.x, .y) paste0(.x, .y)) |>
@@ -318,7 +318,7 @@ ard_categorical_ci.data.frame <- function(data,
   }
 
 
-  df_grouping_cols <- cards::nest_for_ard(data, by = by, include_data = FALSE)
+  df_grouping_cols <- suppressMessages(cards::nest_for_ard(data, by = by, include_data = FALSE))
   levels <- .levels_for_row(data = data, by = by)
 
   suppressMessages(
@@ -413,19 +413,27 @@ ard_categorical_ci.data.frame <- function(data,
     dplyr::mutate(
       ...a_name_anyone_would_ever_pick... =
         dplyr::coalesce(.data$...a_name_anyone_would_ever_pick..., paste(levels, collapse = ""))
-    ) |>
+    )
+
+  # make dummy vector missing if any of the other variables are missing
+  dummy_data[["...a_name_anyone_would_ever_pick..."]][apply(is.na(dummy_data[c(variable, by, strata)]), MARGIN = 1, FUN = any)] <- NA
+
+  # finish processing dummy data
+  dummy_data <- dummy_data |>
     dplyr::select(-any_of(c(variable, by))) %>%
-    {
-      dplyr::bind_cols(
-        .,
-        map(
-          levels,
-          \(level) (.[["...a_name_anyone_would_ever_pick..."]] == level)
-        ) |>
-          stats::setNames(paste0("level_", levels)) |>
-          dplyr::as_tibble()
-      )
-    } |>
+    # styler: off
+    {dplyr::bind_cols(
+      .,
+      map(
+        levels,
+        \(level) {
+          (.[["...a_name_anyone_would_ever_pick..."]] == level)
+        }
+      ) |>
+        stats::setNames(paste0("level_", levels)) |>
+        dplyr::as_tibble()
+    )} |>
+    # styler: on
     dplyr::select(-"...a_name_anyone_would_ever_pick...")
 
   prop_ci_fun <-
