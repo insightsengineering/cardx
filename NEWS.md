2557c6668f8bee367e033a3b4fd1c44f5b1ec856
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2025-04-02 11:09:13 -0400

    Add `ard_stats_mantelhaen_test()` (#246)
    **What changes are proposed in this pull request?**
    * Added function `ard_stats_mantelhaen_test()` for calculating ARDs for
    Cochran-Mantel-Haenszel test results using `stats::mantelhaen.test()`.
    (#238)
    
    Closes #238 
    
    
    --------------------------------------------------------------------------------
    
    Pre-review Checklist (if item does not apply, mark is as complete)
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] PR branch has pulled the most recent updates from master branch:
    `usethis::pr_merge_main()`
    - [ ] If a bug was fixed, a unit test was added.
    - [ ] If a new `ard_*()` function was added, it passes the ARD
    structural checks from `cards::check_ard_structure()`.
    - [ ] If a new `ard_*()` function was added, `set_cli_abort_call()` has
    been set.
    - [ ] If a new `ard_*()` function was added and it depends on another
    package (such as, `broom`), `is_pkg_installed("broom")` has been set in
    the function call and the following added to the roxygen comments:
    `@examplesIf do.call(asNamespace("cardx")$is_pkg_installed, list(pkg =
    "broom""))`
    - [ ] Code coverage is suitable for any new functions/features
    (generally, 100% coverage for new code): `devtools::test_coverage()`
    
    Reviewer Checklist (if item does not apply, mark is as complete)
    
    - [x] If a bug was fixed, a unit test was added.
    - [x] Code coverage is suitable for any new functions/features:
    `devtools::test_coverage()`
    
    When the branch is ready to be merged:
    - [ ] Update `NEWS.md` with the changes from this pull request under the
    heading "`# cardx (development version)`". If there is an issue
    associated with the pull request, reference it in parentheses at the end
    update (see `NEWS.md` for examples).
    - [ ] **All** GitHub Action workflows pass with a :white_check_mark:
    - [ ] Approve Pull Request
    - [ ] Merge the PR. Please use "Squash and merge" or "Rebase and merge".
    
    ---------
    
    Signed-off-by: Emily de la Rua <emily.de_la_rua@contractors.roche.com>
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

--- NEWS.md
+++ NEWS.md
@@ -10,6 +10,8 @@
 
 * Added `cards::as_cards_fun()` to `ard_emmeans_mean_difference()` so when an error occurs the user gets an ARD with the expected ARD structure. (#132)
 
+* Added function `ard_stats_mantelhaen_test()` to calculate ARDs for Cochran-Mantel-Haenszel test results using `stats::mantelhaen.test()`. (#238)
+
 # cardx 0.2.3
 
 ## New Features and Updates
@@ -41,6 +43,8 @@
 * Implemented `summary(extend=TRUE)` in `ard_survival_survfit()` to return results for time points out of bounds. (#224)
 
 * Methods in the {survey} and {survival} packages do not retain inputs variables types in their outputs. We now are able retain these variable types in ARDs returned by `ard_continuous.survey.design()`, `ard_categorical.survey.design()`, `ard_continuous_ci.survey.design()`, `ard_categorical_ci.survey.design()`, and `ard_survival_survfit.data.frame()` (and notably, _not_ in `ard_survival_survfit.survfit()`).
+
+* Added function `ard_stats_mantelhaen_test()` for calculating ARDs for Cochran-Mantel-Haenszel test results using `stats::mantelhaen.test()`. (#238)
 
 # cardx 0.2.1
 
---
+++ R/ard_stats_mantelhaen_test.R
@@ -0,0 +1,108 @@
+#' ARD Cochran-Mantel-Haenszel Chi-Squared Test
+#'
+#' @description
+#' Analysis results data for Cochran-Mantel-Haenszel Chi-Squared Test for count data.
+#' Calculated with `mantelhaen.test(x = data[[variables]], y = data[[by]], z = data[[strata]], ...)`.
+#'
+#' @param data (`data.frame`)\cr
+#'   a data frame.
+#' @param by ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   column name to compare by.
+#' @param variables ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   column names to be compared. Independent tests will be computed for each variable.
+#' @param strata ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   column name to stratify by.
+#' @param ... additional arguments passed to `stats::mantelhaen.test(...)`
+#'
+#' @return ARD data frame
+#' @name ard_stats_mantelhaen_test
+#' @export
+#'
+#' @examplesIf do.call(asNamespace("cardx")$is_pkg_installed, list(pkg = "broom"))
+#' cards::ADSL |>
+#'   ard_stats_mantelhaen_test(by = "ARM", variables = "AGEGR1", strata = "SEX")
+ard_stats_mantelhaen_test <- function(data, by, variables, strata, ...) {
+  set_cli_abort_call()
+
+  # check installed packages ---------------------------------------------------
+  check_pkg_installed("broom")
+
+  # check/process inputs -------------------------------------------------------
+  check_not_missing(data)
+  check_not_missing(variables)
+  check_not_missing(by)
+  check_not_missing(strata)
+  cards::process_selectors(data, by = {{ by }}, variables = {{ variables }}, strata = {{ strata }})
+  check_class(variables, "character")
+  check_scalar(by)
+  check_scalar(strata)
+  check_class(data[[variables]], c("character", "factor"))
+  check_class(data[[by]], c("character", "factor"))
+  check_class(data[[strata]], c("character", "factor"))
+
+  # return empty ARD if no variables selected ----------------------------------
+  if (is_empty(variables)) {
+    return(dplyr::tibble() |> cards::as_card())
+  }
+
+  dots <- dots_list(...)
+  formals_cmh <- formals(asNamespace("stats")[["mantelhaen.test"]])[-c(1:3)]
+  if (!"alternative" %in% names(dots)) formals_cmh$alternative <- "two.sided"
+  mantelhaen.args <- c(dots, formals_cmh[setdiff(names(formals_cmh), names(dots))])
+
+  # build ARD ------------------------------------------------------------------
+  cards::ard_complex(
+    data = data,
+    variables = all_of(variables),
+    statistic = all_of(variables) ~ list(
+      stats_mantelhaen_test = .calc_mantelhaen_test(data, by, variables, strata, mantelhaen.args)
+    )
+  ) |>
+    dplyr::select(-"stat_label") |>
+    dplyr::left_join(
+      .df_mantelhaentest_stat_labels(exact = mantelhaen.args$exact),
+      by = "stat_name"
+    ) |>
+    dplyr::mutate(
+      group1 = by,
+      group2 = strata,
+      stat_label = dplyr::coalesce(.data$stat_label, .data$stat_name),
+      context = "stats_mantelhaen_test",
+    ) |>
+    cards::as_card() |>
+    cards::tidy_ard_column_order() |>
+    cards::tidy_ard_row_order()
+}
+
+.calc_mantelhaen_test <- function(data, by, variables, strata, mantelhaen.args) {
+  cards::as_cards_fn(
+    \(x, data, variables, ...) {
+      stats::mantelhaen.test(
+        x = x,
+        y = data[[by]],
+        z = data[[strata]],
+        mantelhaen.args
+      ) |>
+        broom::tidy() |>
+        dplyr::bind_cols(mantelhaen.args)
+    },
+    stat_names = c(
+      "estimate", "statistic", "p.value", "parameter", "correct", "exact", "conf.level", "conf.low", "conf.high"
+    )
+  )
+}
+
+.df_mantelhaentest_stat_labels <- function(exact = FALSE) {
+  dplyr::tribble(
+    ~stat_name, ~stat_label,
+    "estimate", ifelse(exact, "Mantel-Haenszel Odds Ratio Estimate", "Conditional Maximum Likelihood Odds Ratio Estimate"),
+    "statistic", ifelse(exact, "Mantel-Haenszel X-squared Statistic", "Generalized Cochran-Mantel-Haenszel Statistic"),
+    "p.value", "p-value",
+    "parameter", "Degrees of Freedom",
+    "correct", "Continuity Correction",
+    "exact", "Exact Conditional Test",
+    "conf.level", "CI Confidence Level",
+    "conf.low", "CI Lower Bound",
+    "conf.high", "CI Upper Bound"
+  )
+}
